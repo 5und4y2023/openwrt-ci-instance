92f6a6376ac3fb52330eff25aef75168266bdd6a
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-09-02 13:09:57 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -31,7 +31,7 @@ sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 #sed -i 's/root::0:0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 #sed -i 's/root:::0:99999:7:::/root:$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.:0:0:99999:7:::/g' /etc/shadow
 
-#cp /etc/my-clash /etc/openclash/core/clash_meta
+cp /etc/my-clash /etc/openclash/core/clash_meta
 
 
 /etc/init.d/network restart
