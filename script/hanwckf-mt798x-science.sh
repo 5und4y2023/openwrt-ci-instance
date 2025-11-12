86116f6b16c1b8cf15caf04eaa0c88886c6e7191
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2025-11-12 23:50:11 +0800

    Update hanwckf-mt798x-science.sh

--- script/hanwckf-mt798x-science.sh
+++ script/hanwckf-mt798x-science.sh
@@ -87,12 +87,12 @@ rm -rf package/small-package
 
 rm -rf feeds/packages/net/frp
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/packages package/imm24pkg
-mv package/imm24pkg/net/frp package/frp
+mv package/imm24pkg/net/frp feeds/packages/net/frp
 rm -rf package/imm24pkg
 
 rm -rf feeds/luci/applications/luci-app-frpc
 git clone --depth 1 -b openwrt-24.10 https://github.com/immortalwrt/luci package/imm24luci
-mv package/imm24luci/applications/luci-app-frpc package/luci-app-frpc
+mv package/imm24luci/applications/luci-app-frpc feeds/luci/applications/luci-app-frpc
 rm -rf package/imm24luci
 
 git clone --depth 1 https://github.com/coolsnowwolf/lede.git package/lede
