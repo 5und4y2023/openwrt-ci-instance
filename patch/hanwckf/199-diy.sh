8c3888965cd502cfed36e6f9faab68ce705c95af
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-03-27 14:46:06 +0800

    Update 199-diy.sh

--- patch/hanwckf/199-diy.sh
+++ patch/hanwckf/199-diy.sh
@@ -21,16 +21,16 @@ uci add_list dhcp.lan.ra_flags='none'
 uci set network.lan.delegate='0'
 uci set network.lan.ip6ifaceid='random'
 
-uci set network.lan.ipaddr=192.168.140.1
+#uci set network.lan.ipaddr=192.168.140.1
 uci commit dhcp
 uci commit network
-
-uci set wireless.default_MT7981_1_1.ssid=Open2G
+WIFINAME=$(ip link show br-lan 2>/dev/null | awk '/link\/ether/{split($2,m,":");print toupper(m[5]m[6])}')
+uci set wireless.default_MT7981_1_1.ssid=WiFi-$WIFINAME-2.4G
 uci set wireless.default_MT7981_1_1.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_1.key=open12345
-uci set wireless.default_MT7981_1_2.ssid=Open5G
+uci set wireless.default_MT7981_1_1.key=1234qwer+-
+uci set wireless.default_MT7981_1_2.ssid=WiFi-$WIFINAME-5G
 uci set wireless.default_MT7981_1_2.encryption=psk2+ccmp
-uci set wireless.default_MT7981_1_2.key=open12345
+uci set wireless.default_MT7981_1_2.key=1234qwer+-
 uci commit wireless
 
 uci commit
@@ -39,8 +39,8 @@ sed -i '/ssrp/d' /etc/opkg/distfeeds.conf
 sed -i '/helloworld/d' /etc/opkg/distfeeds.conf
 sed -i '/passwall/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
-sed -i 's/root::0:0:99999:7:::/root:$1$P.dQe2I4$BO.thcMA9OYlt8R8ECnnx1:0:0:99999:7:::/g' /etc/shadow
-sed -i 's/root:::0:99999:7:::/root:$1$P.dQe2I4$BO.thcMA9OYlt8R8ECnnx1:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root::0:0:99999:7:::/root:$1$P.dQe2I4$BO.thcMA9OYlt8R8ECnnx1:0:0:99999:7:::/g' /etc/shadow
+#sed -i 's/root:::0:99999:7:::/root:$1$P.dQe2I4$BO.thcMA9OYlt8R8ECnnx1:0:0:99999:7:::/g' /etc/shadow
 
 
 /etc/init.d/network restart
