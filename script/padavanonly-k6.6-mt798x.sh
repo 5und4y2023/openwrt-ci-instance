b47f755d3be25ca81ce1a23924494f0c2428fbca
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-03-29 21:35:20 +0800

    Update padavanonly-k6.6-mt798x.sh

--- script/padavanonly-k6.6-mt798x.sh
+++ script/padavanonly-k6.6-mt798x.sh
@@ -1,11 +1,10 @@
-sed -i 's/192.168.6.1/10.3.2.1/g' package/base-files/files/bin/config_generate
-sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.3.2.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
+sed -i 's/192.168.6.1/10.1.1.1/g' package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/10.1.1.1/g" $(find ./feeds/luci/modules/luci-mod-system/ -type f -name "flash.js")
 #sed -i 's/ImmortalWrt/OpenWrt/g' package/base-files/files/bin/config_generate
-#sed -i 's#mirrors.vsean.net/openwrt#mirror.nju.edu.cn/immortalwrt#g' package/emortal/default-settings/files/99-default-settings-chinese
 #mv $GITHUB_WORKSPACE/patch/banner package/base-files/files/etc/banner
 
-mv $GITHUB_WORKSPACE/patch/padavanonly/199-diy-wifi.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
-mv $GITHUB_WORKSPACE/patch/padavanonly/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
+mv $GITHUB_WORKSPACE/patch/padavanonly/199-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
+#mv $GITHUB_WORKSPACE/patch/padavanonly/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 
 if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
     echo "✅ 已选择 luci-app-openclash，添加 openclash core"
@@ -53,7 +52,7 @@ git clone --depth 1 https://github.com/zzsj0928/luci-app-pushbot.git package/luc
 git clone --depth 1 https://github.com/AngelaCooljx/luci-theme-material3.git package/luci-theme-material3
 
 rm -rf feeds/packages/net/{adguardhome,alist,tailscale}
-git clone --depth 1 https://github.com/kenzok8/small-package.git package/small-package
+git clone --depth 1 https://github.com/kenzok8/jell.git package/small-package
 mv package/small-package/adguardhome package/adguardhome
 mv package/small-package/luci-app-adguardhome package/luci-app-adguardhome
 mv package/small-package/luci-app-guest-wifi package/luci-app-guest-wifi
@@ -64,12 +63,6 @@ mv package/small-package/wrtbwmon package/wrtbwmon
 mv package/small-package/luci-app-wrtbwmon package/luci-app-wrtbwmon
 rm -rf package/small-package
 
-#下载5g模块
-#git clone --depth=1 https://github.com/Siriling/5G-Modem-Support.git package/5g-modem
-#rm -rf feeds/packages/net/quectel-cm
-#rm -rf feeds/packages/kernel/fibocom-qmi-wwan
-#rm -rf feeds/packages/kernel/quectel-qmi-wwan
-#rm -rf feeds/luci/protocols/luci-proto-quectel
 
 # iStore
 git clone --depth 1 https://github.com/linkease/istore.git package/istore
@@ -78,24 +71,6 @@ git clone --depth 1 https://github.com/linkease/nas-packages-luci.git package/na
 mv package/nas-packages/network/services/* package/nas-packages/
 rm -rf package/nas-packages/network
 
-#git clone --depth 1 -b openwrt-21.02 https://github.com/immortalwrt/luci.git package/imm21-luci
-#mv package/imm21-luci/applications/luci-app-accesscontrol package/luci-app-accesscontrol
-#mv package/imm21-luci/applications/luci-app-filetransfer package/luci-app-filetransfer
-#mv package/imm21-luci/applications/luci-app-v2ray-server package/luci-app-v2ray-server
-#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-accesscontrol/Makefile
-#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-filetransfer/Makefile
-#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-v2ray-server/Makefile
-#rm -rf package/imm21-luci
-#git clone --depth 1 -b openwrt-23.05 https://github.com/immortalwrt/luci.git package/imm23-luci
-#mv package/imm23-luci/applications/luci-app-accesscontrol package/luci-app-accesscontrol
-#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-accesscontrol/Makefile
-#rm -rf package/imm23-luci
-
-
-#git clone --depth 1 -b master https://github.com/coolsnowwolf/luci.git package/lean-luci
-#mv package/lean-luci/applications/luci-app-arpbind package/luci-app-arpbind
-#sed -i 's#../../luci.mk#$(TOPDIR)/feeds/luci/luci.mk#g' package/luci-app-arpbind/Makefile
-#rm -rf package/lean-luci
 
 ## 修改DTS的ubi为490MB的0x1ea00000
 #sed -i 's/reg = <0x600000 0x6e00000>/reg = <0x600000 0x1ea00000>/g' target/linux/mediatek/files-5.4/arch/arm64/boot/dts/mediatek/mt7986a-xiaomi-redmi-router-ax6000.dts
