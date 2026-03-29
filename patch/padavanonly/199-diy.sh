b0b345b76a75cc42196b7953d632e267fddb065a
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-03-29 21:36:24 +0800

    Update 199-diy.sh

--- patch/padavanonly/199-diy.sh
+++ patch/padavanonly/199-diy.sh
@@ -1,5 +1,80 @@
 #!/bin/sh
 
+# 提取MAC地址
+# br-lan
+FRPMAC=$(ip link show br-lan | awk '/link\/ether/ {print $2}')
+
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(cat /sys/class/net/br-lan/address 2>/dev/null)
+fi
+
+# eth0 和 wan
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(cat /sys/class/net/eth0/address 2>/dev/null)
+fi
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(ip link show wan | awk '/link\/ether/ {print $2}')
+fi
+
+
+# 如果还不行，尝试获取第一个有MAC地址的接口
+if [ -z "$FRPMAC" ]; then
+    for iface in /sys/class/net/*/address; do
+        FRPMAC=$(cat "$iface" 2>/dev/null | grep -v "00:00:00:00:00:00" | head -1)
+        [ -n "$FRPMAC" ] && break
+    done
+fi
+
+# 4. 处理MAC地址：去掉冒号并转大写
+FRPMAC_CLEAN=$(echo "$FRPMAC" | tr -d ':' | tr 'a-f' 'A-F')
+
+# 5. 组合成FRPNAME
+FRPNAME="${FRPMAC_CLEAN}"
+
+
+#echo "处理后MAC: $FRPMAC_CLEAN"
+#echo "最终FRPNAME: $FRPNAME"
+
+uci set frpc.common.server_addr='frp.jcmeng.top'
+uci set frpc.common.server_port='40101'
+uci set frpc.common.token='frp2026+-*.'
+uci set frpc.common.tls_enable='false'
+
+uci set frpc.common.admin_addr='127.0.0.1'
+uci set frpc.common.admin_port='19698'
+uci set frpc.common.admin_user='frpc'
+uci set frpc.common.admin_pwd='1234qwer+-'
+
+uci add frpc conf
+uci set frpc.@conf[-1].name="${FRPNAME}_luci"
+uci set frpc.@conf[-1].type='tcp'
+uci set frpc.@conf[-1].use_encryption='true'
+uci set frpc.@conf[-1].use_compression='true'
+uci set frpc.@conf[-1].local_ip='127.0.0.1'
+uci set frpc.@conf[-1].local_port='80'
+uci set frpc.@conf[-1].remote_port='0'
+
+uci add frpc conf
+uci set frpc.@conf[-1].name="${FRPNAME}_clash"
+uci set frpc.@conf[-1].type='tcp'
+uci set frpc.@conf[-1].use_encryption='true'
+uci set frpc.@conf[-1].use_compression='true'
+uci set frpc.@conf[-1].local_ip='127.0.0.1'
+uci set frpc.@conf[-1].local_port='9090'
+uci set frpc.@conf[-1].remote_port='0'
+
+uci add frpc conf
+uci set frpc.@conf[-1].name="${FRPNAME}_frpweb"
+uci set frpc.@conf[-1].type='tcp'
+uci set frpc.@conf[-1].use_encryption='true'
+uci set frpc.@conf[-1].use_compression='true'
+uci set frpc.@conf[-1].local_ip='127.0.0.1'
+uci set frpc.@conf[-1].local_port='19698'
+uci set frpc.@conf[-1].remote_port='0'
+
+uci commit
+/etc/init.d/frpc restart
+
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
