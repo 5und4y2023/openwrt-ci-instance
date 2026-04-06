9d7c9e16fbe2bf856ad40540b944e0fcd299fe58
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-04-06 15:38:25 +0800

    Update 199-diy.sh

--- patch/padavanonly/199-diy.sh
+++ patch/padavanonly/199-diy.sh
@@ -1,80 +1,5 @@
 #!/bin/sh
 
-# 提取MAC地址
-# br-lan
-FRPMAC=$(ip link show br-lan | awk '/link\/ether/ {print $2}')
-
-if [ -z "$FRPMAC" ]; then
-    FRPMAC=$(cat /sys/class/net/br-lan/address 2>/dev/null)
-fi
-
-# eth0 和 wan
-if [ -z "$FRPMAC" ]; then
-    FRPMAC=$(cat /sys/class/net/eth0/address 2>/dev/null)
-fi
-if [ -z "$FRPMAC" ]; then
-    FRPMAC=$(ip link show wan | awk '/link\/ether/ {print $2}')
-fi
-
-
-# 如果还不行，尝试获取第一个有MAC地址的接口
-if [ -z "$FRPMAC" ]; then
-    for iface in /sys/class/net/*/address; do
-        FRPMAC=$(cat "$iface" 2>/dev/null | grep -v "00:00:00:00:00:00" | head -1)
-        [ -n "$FRPMAC" ] && break
-    done
-fi
-
-# 4. 处理MAC地址：去掉冒号并转大写
-FRPMAC_CLEAN=$(echo "$FRPMAC" | tr -d ':' | tr 'a-f' 'A-F')
-
-# 5. 组合成FRPNAME
-FRPNAME="${FRPMAC_CLEAN}"
-
-
-#echo "处理后MAC: $FRPMAC_CLEAN"
-#echo "最终FRPNAME: $FRPNAME"
-
-uci set frpc.common.server_addr='frp.jcmeng.top'
-uci set frpc.common.server_port='40101'
-uci set frpc.common.token='frp2026+-*.'
-uci set frpc.common.tls_enable='false'
-
-uci set frpc.common.admin_addr='127.0.0.1'
-uci set frpc.common.admin_port='19698'
-uci set frpc.common.admin_user='frpc'
-uci set frpc.common.admin_pwd='1234qwer+-'
-
-uci add frpc conf
-uci set frpc.@conf[-1].name="${FRPNAME}_luci"
-uci set frpc.@conf[-1].type='tcp'
-uci set frpc.@conf[-1].use_encryption='true'
-uci set frpc.@conf[-1].use_compression='true'
-uci set frpc.@conf[-1].local_ip='127.0.0.1'
-uci set frpc.@conf[-1].local_port='80'
-uci set frpc.@conf[-1].remote_port='0'
-
-uci add frpc conf
-uci set frpc.@conf[-1].name="${FRPNAME}_clash"
-uci set frpc.@conf[-1].type='tcp'
-uci set frpc.@conf[-1].use_encryption='true'
-uci set frpc.@conf[-1].use_compression='true'
-uci set frpc.@conf[-1].local_ip='127.0.0.1'
-uci set frpc.@conf[-1].local_port='9090'
-uci set frpc.@conf[-1].remote_port='0'
-
-uci add frpc conf
-uci set frpc.@conf[-1].name="${FRPNAME}_frpweb"
-uci set frpc.@conf[-1].type='tcp'
-uci set frpc.@conf[-1].use_encryption='true'
-uci set frpc.@conf[-1].use_compression='true'
-uci set frpc.@conf[-1].local_ip='127.0.0.1'
-uci set frpc.@conf[-1].local_port='19698'
-uci set frpc.@conf[-1].remote_port='0'
-
-uci commit
-/etc/init.d/frpc restart
-
 # 设置所有网口可访问网页终端
 uci delete ttyd.@ttyd[0].interface
 
@@ -85,16 +10,6 @@ uci set dropbear.@dropbear[0].Interface=''
 #uci commit luci
 
 #uci set network.lan.ipaddr='192.168.6.1'
-uci set network.lan.ip6assign=64
-uci del network.globals.ula_prefix
-uci set dhcp.lan.dns_service='0'
-uci del dhcp.lan.dhcpv6
-uci del dhcp.lan.ndp
-uci set dhcp.lan.ra='server'
-uci del dhcp.lan.ra_flags
-uci add_list dhcp.lan.ra_flags='none'
-uci set network.lan.delegate='0'
-uci set network.lan.ip6ifaceid='random'
 
 uci commit dhcp
 uci commit network
@@ -123,7 +38,6 @@ sed -i '$a src/gz openwrt_kmods https://mirrors.vsean.net/openwrt/releases/24.10
 #sed -i 's/root:::0:99999:7:::/root:$1$0kv2aZ4P$WkI.7M.V1N6WSEDahJwot.:0:0:99999:7:::/g' /etc/shadow
 
 /etc/init.d/network restart
-/etc/init.d/odhcpd restart
-/etc/init.d/rpcd restart
+
 
 exit 0
