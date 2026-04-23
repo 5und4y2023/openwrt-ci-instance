ca811f681bb7e950439aae111282dfd5cb6434a5
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-04-23 15:11:51 +0800

    Update 199-frp.sh

--- patch/hanwckf/199-frp.sh
+++ patch/hanwckf/199-frp.sh
@@ -1,4 +1,31 @@
 #!/bin/sh
+# 提取MAC地址
+# br-lan
+FRPMAC=$(ip link show br-lan | awk '/link\/ether/ {print $2}')
+
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(cat /sys/class/net/br-lan/address 2>/dev/null)
+fi
+
+# eth0 和 wan
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(cat /sys/class/net/eth0/address 2>/dev/null)
+fi
+if [ -z "$FRPMAC" ]; then
+    FRPMAC=$(ip link show wan | awk '/link\/ether/ {print $2}')
+fi
+
+
+# 如果还不行，尝试获取第一个有MAC地址的接口
+if [ -z "$FRPMAC" ]; then
+    for iface in /sys/class/net/*/address; do
+        FRPMAC=$(cat "$iface" 2>/dev/null | grep -v "00:00:00:00:00:00" | head -1)
+        [ -n "$FRPMAC" ] && break
+    done
+fi
+
+# 4. 处理MAC地址：去掉冒号并转大写
+FRPNAME=$(echo "$FRPMAC" | tr -d ':' | tr 'a-f' 'A-F')
 
 uci del frpc.ssh
 uci set frpc.common.server_addr='frp.jcmeng.top'
