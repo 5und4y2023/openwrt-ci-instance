3551ed235db538c723cd2888575d86c88a285ca6
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-04-24 12:34:50 +0800

    Update hanwckf-mt798x.sh

--- script/hanwckf-mt798x.sh
+++ script/hanwckf-mt798x.sh
@@ -11,7 +11,7 @@ sed -i 's/ImmortalWrt/Router/g' package/base-files/files/bin/config_generate
 #mv $GITHUB_WORKSPACE/patch/hanwckf/mtwifi.sh package/mtk/applications/mtwifi-cfg/files/mtwifi.sh
 mv $GITHUB_WORKSPACE/patch/hanwckf/199-diy.sh package/base-files/files/etc/uci-defaults/zz-diy.sh
 if grep -Eq "frpc=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
-	mv $GITHUB_WORKSPACE/hanwckf/199-frp.sh package/base-files/files/etc/uci-defaults/za.sh
+	mv $GITHUB_WORKSPACE/patch/hanwckf/199-frp.sh package/base-files/files/etc/uci-defaults/za.sh
 fi
 
 if grep -q "openclash=y" "$GITHUB_WORKSPACE/$CONFIG_FILE"; then
