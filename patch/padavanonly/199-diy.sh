f61f6f8f103468842a98b9e65b379e8da9e1ce0b
5und4y2023 <164324407+5und4y2023@users.noreply.github.com>
2026-06-24 15:34:36 +0800

    Update 199-diy.sh

--- patch/padavanonly/199-diy.sh
+++ patch/padavanonly/199-diy.sh
@@ -31,13 +31,13 @@ sed -i '/Modem/d' /etc/opkg/distfeeds.conf
 sed -ri '/check_signature/s@^[^#]@#&@' /etc/opkg.conf
 sed -i '/filogic/d' /etc/opkg/distfeeds.conf
 sed -i '$a src/gz openwrt_core https://mirrors.vsean.net/openwrt/releases/24.10-SNAPSHOT/targets/mediatek/filogic/packages' /etc/opkg/distfeeds.conf
-sed -i '$a src/gz openwrt_kmods https://mirrors.vsean.net/openwrt/releases/24.10-SNAPSHOT/targets/mediatek/filogic/kmods/6.6.95-1-3ca4b8cb2fcc3a2027e8496143a86cab' /etc/opkg/distfeeds.conf
+sed -i '$a src/gz openwrt_kmods https://mirrors.vsean.net/openwrt/releases/24.10-SNAPSHOT/targets/mediatek/filogic/kmods/6.6.133-1-a8b93917f464536104594f27d870028d' /etc/opkg/distfeeds.conf
 #sed -i '$a #src/gz kiddin9 https://dl.openwrt.ai/packages-24.10/aarch64_generic/kiddin9' /etc/opkg/customfeeds.conf
 
 #sed -i 's/root::0:0:99999:7:::/root:$1$0kv2aZ4P$WkI.7M.V1N6WSEDahJwot.:0:0:99999:7:::/g' /etc/shadow
 #sed -i 's/root:::0:99999:7:::/root:$1$0kv2aZ4P$WkI.7M.V1N6WSEDahJwot.:0:0:99999:7:::/g' /etc/shadow
 
-/etc/init.d/network restart
+#/etc/init.d/network restart
 
 
 exit 0
